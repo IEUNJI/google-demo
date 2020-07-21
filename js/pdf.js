9d8a0abde928bde646077ffd0a7f35a2795a692c
linzijun <linzjk@yonyou.com>
2020-07-21 15:47:11 +0800

    增加pdf页面

---
+++ js/pdf.js
@@ -0,0 +1,14 @@
+import openConsole from '../utils/openConsole.js';
+
+class PdfPage {
+  constructor() {
+
+  }
+
+  init = () => {
+    openConsole();
+  }
+}
+
+const pdfPage = new PdfPage();
+pdfPage.init();
