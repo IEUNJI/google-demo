67cec611b7c7a785d2c112291da031b7b14635e6
linzijun <linzjk@yonyou.com>
2020-07-29 15:12:38 +0800

    增加battery页面

---
+++ js/battery.js
@@ -0,0 +1,34 @@
+import openConsole from '../utils/openConsole.js';
+
+class BatteryPage {
+  constructor() {
+    this.charging = document.querySelector('#charging');
+    this.level = document.querySelector('#level');
+  }
+
+  batteryHandler = battery => {
+    this.charging.innerHTML = `${battery.charging ? '充电中' : '未充电'}`;
+    this.level.innerHTML = `${Number(battery.level.toFixed(2)) * 100}%`;
+  }
+
+  bindEvents = async () => {
+    const battery = await navigator.getBattery();
+    this.batteryHandler(battery);
+
+    battery.addEventListener('chargingchange', event => {
+      this.batteryHandler(event.target);
+    });
+
+    battery.addEventListener('levelchange', event => {
+      this.batteryHandler(event.target);
+    });
+  }
+
+  init = () => {
+    openConsole();
+    this.bindEvents();
+  }
+}
+
+const batteryPage = new BatteryPage();
+batteryPage.init();
