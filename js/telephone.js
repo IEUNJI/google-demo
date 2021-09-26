01dae4cb4f34aa7d38787d21fb1af99632b0bc8a
林子峻 <linzijun1@jd.com>
2021-09-26 13:59:36 +0800

    增加拨号页面

---
+++ js/telephone.js
@@ -0,0 +1,14 @@
+import openConsole from '../utils/openConsole.js';
+
+class TelephonePage {
+  constructor() {
+    
+  }
+
+  init = () => {
+    openConsole();
+  }
+}
+
+const telephonePage = new TelephonePage();
+telephonePage.init();
